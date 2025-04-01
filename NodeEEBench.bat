da4fdf77bbd32ad5a0018850c21eb09cbbda2381
stsimayr <stsimayr@hochschulekempten.onmicrosoft.com>
2025-04-01 14:26:37 +0200

    geänderte Batchdatei commited

--- NodeEEBench.bat
+++ NodeEEBench.bat
@@ -1,5 +1,28 @@
-REM Starting nodejs with webserver
+@echo off
+setlocal enabledelayedexpansion
 
+:: Nach einem Verzeichnis suchen, das mit "NodeEEBench" beginnt
+for /d /r C:\temp %%D in (NodeEEBench*) do (
+    if exist "%%D\node" (
+        set "NODE_DIR=%%D\node"
+        goto :found
+    )
+)
+
+:: Falls nicht gefunden, Fehlermeldung ausgeben und beenden
+echo Fehler: Kein passender NodeEEBench-Ordner mit node-Verzeichnis gefunden.
+pause
+exit /b 1
+
+:found
+echo Gefundener Node-Pfad: %NODE_DIR%
+
+:: Node.js zum PATH hinzufügen
+set "PATH=%PATH%;%NODE_DIR%"
+
+:: Node-Server starten
+echo Starte Node.js-Server...
 node ServerEEBench.js
 
+:: Konsole offen halten
 pause
